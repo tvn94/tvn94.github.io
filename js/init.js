f25ec2c4d197abbbf3fe3ccabb9321d79fa97d46
tvn194 <tvn@users.noreply.github.com>
2020-06-01 16:31:38 +0700

    thu

--- js/init.js
+++ js/init.js
@@ -15,12 +15,12 @@
 				prefix: 'css/style',
 				resetCSS: true,
 				boxModel: 'border',
-				containers: 1200,
+				containers: '100%',
 				useOrientation: true,
 				breakpoints: {
-					'widest': { range: '*', containers: 1360, grid: { gutters: 50 }, hasStyleSheet: false },
-					'wide': { range: '-1680', containers: 1200, grid: { gutters: 40 } },
-					'normal': { range: '-1280', containers: 960, grid: { gutters: 30 }, lockViewport: true },
+					'widest': { range: '*', containers: '100%', grid: { gutters: 50 }, hasStyleSheet: false },
+					'wide': { range: '-1680', containers: '100%', grid: { gutters: 40 } },
+					'normal': { range: '-1280', containers: '100%', grid: { gutters: 30 }, lockViewport: true },
 					'narrow': { range: '-1000', containers: '100%', grid: { gutters: 25, collapse: true }, lockViewport: true },
 					'mobile': { range: '-640', containers: '100%', grid: { gutters: 10, collapse: true }, lockViewport: true }
 				}
